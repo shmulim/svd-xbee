b223336351d7fb50e26bdccbc5d894282c952be7
Jan Kolkmeier <jankolkmeier@gmail.com>
2013-03-17 00:37:25 +0100

    Added new examples

---
+++ examples/nutshell/nutshell.js
@@ -0,0 +1,14 @@
+var XBee = require('../../index.js').XBee;
+
+var xbee = new XBee({
+  port: 'COM3',   // replace with yours
+  baudrate: 9600 // 9600 is default
+})
+
+// Add Set to your remote node
+var robot = xbee.addNode([0x00,0x13,0xa2,0x00,0x40,0x61,0x2f,0xe4]);
+
+var robot.on("data", function(data) {
+  console.log("robot>", data);
+  if (data == "ping") robot.send("pong");
+});
